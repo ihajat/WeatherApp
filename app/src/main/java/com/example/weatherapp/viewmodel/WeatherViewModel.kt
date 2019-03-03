5714c34fbc6bb9887c11340b7b1e1d2ac833ff6b
ihajat <ihajat@hotmail.com>
2019-03-03 10:48:27 +0000

    2) write code to pass the failing test to validate a correct latitude

--- app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
@@ -4,6 +4,7 @@ import java.text.DecimalFormat
 
 class WeatherViewModel {
     private val LONGITUDE_PATTERN = "^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$"
+    private val LATITUDE_PATTERN = "^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$"
     private val df = DecimalFormat("#.######")
 
     fun validateLongitude(longitudeToTest: Double ): Boolean {
@@ -16,5 +17,13 @@ class WeatherViewModel {
         return false
     }
 
-
+    fun validateLatitude(latitudeToTest: Double ): Boolean {
+        try{
+            return df.format(latitudeToTest).matches(LATITUDE_PATTERN.toRegex())
+        }
+        catch (ex: IllegalArgumentException ){
+            return false
+        }
+        return false
+    }
 }
